a1860cb5fa6276dc4d07d25d9b530a83f597aa2b
Edxael <zerphyr.r@gmail.com>
2017-09-19 22:16:38 -0600

    add files

---
+++ 04-Helio/03-Capstone/01-Store/00-ReadMe.txt
@@ -0,0 +1,15 @@
+This is one Full-Stack Project.
+
+This project is one online store where users can review items and and purchase them.
+
+the technologies used on this Web-Applications are:
+
+Node.js
+Express,js
+MongoDB
+
+
+
+    ---  Edmundo Rubio  ---
+Portfolio: www.mycode.website
+Resume: www.edmundorubio.site
--- 04-Helio/03-Capstone/01-Store/app.js
+++ 04-Helio/03-Capstone/01-Store/app.js
@@ -4,6 +4,29 @@ var morgan = require('morgan');
 var ejs = require('ejs');
 
 
+
 app.set('view enginge', 'ejs');
 app.use(morgan('dev'));
-app.use(express.static(__dirname + ''));
+app.use(express.static(__dirname + '/node_modules/bootstrap/dist'));
+app.use(express.static(__dirname + '/view'));
+
+
+
+app.get('/', function(req, res){
+  res.render('pages/00-index.ejs');
+});
+
+app.get('/1', function(req, res){
+  res.render('pages/01-one.ejs');
+});
+
+
+
+
+app.listen(3000, function(err){
+  if(err){ throw err; }
+  console.log("");
+  console.log("Server Up and Running");
+  console.log("");
+  console.log("");
+});
