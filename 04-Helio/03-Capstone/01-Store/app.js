f1f9f08015e638700746c80a4d761d5eb8f594cb
Edmundo Rubio <zerphyr.r@gmail.com>
2017-09-20 20:37:39 -0600

    add itmes for the class

--- 04-Helio/03-Capstone/01-Store/app.js
+++ 04-Helio/03-Capstone/01-Store/app.js
@@ -8,7 +8,8 @@ var ejs = require('ejs');
 app.set('view enginge', 'ejs');
 app.use(morgan('dev'));
 app.use(express.static(__dirname + '/node_modules/bootstrap/dist'));
-app.use(express.static(__dirname + '/view'));
+app.use(express.static(__dirname + '/views'));
+
 
 
 
@@ -20,13 +21,17 @@ app.get('/1', function(req, res){
   res.render('pages/01-one.ejs');
 });
 
+app.get('/2', function(req, res){
+  res.render('pages/02-two.ejs');
+});
+
 
 
 
 app.listen(3000, function(err){
   if(err){ throw err; }
   console.log("");
-  console.log("Server Up and Running");
+  console.log("Server Up and Running...");
   console.log("");
   console.log("");
 });
