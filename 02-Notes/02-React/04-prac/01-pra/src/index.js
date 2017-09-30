9bbfd91fb01da9767d9ab5909d9081cb1f4b746d
Edxael <zerphyr.r@gmail.com>
2017-09-29 19:48:36 -0600

    add study notes

---
+++ 02-Notes/02-React/04-prac/01-pra/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+const MainCont = () => {
+  return(
+    <div className="MCStyle">
+      <h3>Hello World</h3>
+    </div>
+  )
+}
+
+
+ReactDOM.render(<MainCont />, document.getElementById("root"));
--- 04-Helio/02-Clas-Code/05-react-app/src/01-comps/01-comp.jsx
+++ 04-Helio/02-Clas-Code/05-react-app/src/01-comps/01-comp.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 const Com1 = () => {
   return(
     <h2> This is a stateless component </h2>
+    <div className="one"></div>
   )
 }
 
