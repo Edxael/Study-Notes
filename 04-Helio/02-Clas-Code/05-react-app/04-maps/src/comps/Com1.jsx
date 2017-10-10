40bfe00de3434a5c43db2a9e597a51c06e22f99b
Edxael <zerphyr.r@gmail.com>
2017-10-10 16:13:56 -0600

    add notes

---
+++ 04-Helio/02-Clas-Code/05-react-app/04-maps/src/comps/Com1.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class MapComp extends React.Component {
+  render() {
+    return(
+      <div>
+        <p>Map Comp here</p>
+      </div>
+    )
+  }
+}
+
+export default MapComp;
