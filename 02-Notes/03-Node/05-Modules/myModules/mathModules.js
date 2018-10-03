807f179fd0efefca38a79b1928e05af783a5cbe7
Edmundo rubio <edmundo@webaholics.co>
2018-10-03 00:02:53 -0600

    node modules

---
+++ 02-Notes/03-Node/05-Modules/myModules/mathModules.js
@@ -0,0 +1,21 @@
+// module.exports = {
+const chuntaro = {
+    mySum: (num1, num2) => {
+        return num1 + num2
+    },
+
+    mySub: (num1, num2) => {
+        return num1 - num2
+    },
+
+    myMul: (num1,  num2) => {
+        return num1 * num2
+    },
+
+    myDiv: (num1,  num2) => {
+        return num1 / num2
+    }
+}
+
+
+module.exports = chuntaro
---
+++ 02-Notes/03-Node/05-Modules/server.js
@@ -0,0 +1,9 @@
+const ERP =  require('./myModules/mathModules')
+
+
+console.log("  Executing 'main()'")
+
+let data1 = 5
+let data2 = 14
+
+console.log( "The sum", ERP.mySum(data1, data2) )
