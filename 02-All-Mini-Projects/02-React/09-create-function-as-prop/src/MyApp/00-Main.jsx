53da184f04ccfba7265a9f5cf92e7338cf5c8ed8
Edmundo rubio <edmundo@webaholics.co>
2018-10-15 23:51:20 -0600

    passing function as props

---
+++ 02-All-Mini-Projects/02-React/09-create-function-as-prop/src/MyApp/00-Main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from './components/01-Header'
+import Body from './components/02-Body'
+import Footer from './components/03-Foooter'
+
+class Main1 extends React.Component{
+    render(){
+        return(
+            <div>
+                <Header />
+                <Body />
+                <Footer />
+            </div>
+        )
+    }
+}
+
+export default Main1
---
+++ 02-All-Mini-Projects/02-React/09-create-function-as-prop/src/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Main1 from './MyApp/00-Main'
+
+ReactDOM.render(<Main1 />, document.getElementById('root'))
