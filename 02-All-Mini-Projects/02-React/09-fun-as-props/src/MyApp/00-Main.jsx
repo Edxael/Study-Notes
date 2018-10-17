e837db29bc3ee3fb4c12b6ac0eec811e8c2d06a7
Edmundo rubio <edmundo@webaholics.co>
2018-10-16 21:16:45 -0600

    update

---
+++ 02-All-Mini-Projects/02-React/09-fun-as-props/src/MyApp/00-Main.jsx
@@ -0,0 +1,15 @@
+// Main
+
+import React, { Component } from 'react';
+
+class Main extends Component {
+  render() {
+    return (
+      <div>
+        <p>Tthis is the main component</p>
+      </div>
+    );
+  }
+}
+
+export default Main;
