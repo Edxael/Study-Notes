68ab664a3c002bb0a02259a117a78c0411656676
Edmundo rubio <edmundo@webaholics.co>
2018-10-24 22:37:30 -0600

    update

---
+++ 02-All-Mini-Projects/02-React/00-Prac/01-p/src/04-parent.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Parent extends React.Component{
+  render(){
+    return(
+      <div>
+          <p>Parent component</p>
+      </div>
+    );
+  }
+}
+
+export default Parent;
---
+++ 02-All-Mini-Projects/02-React/00-Prac/01-p/src/code.jsx
@@ -0,0 +1,15 @@
+<Card>
+  {{
+    header: 'Shiba Inu',
+    media: <img src="examples/shiba2.jpg" />,
+    content: (
+      <p>The Shiba Inu is the smallest of the six original ...</p>
+    ),
+    actions: (
+      <>
+        <button>Like</button>
+        <button>Share</button>
+      </>
+    ),
+   }}
+</Card>
