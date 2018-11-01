33a1f57bd5d5ad9aa5c46bb27082634146207742
Edmundo rubio <edmundo@webaholics.co>
2018-10-31 23:38:41 -0600

    notes on GraphQL

---
+++ 02-All-Mini-Projects/07-Redux/00-redux-test/src/App/PAGES/02-API-Call.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class APICall extends Component{
+  render(){
+    return(
+      <div>
+        <p>APICall</p>
+      </div>
+    )
+  }
+}
+
+export default APICall;
---
+++ 02-All-Mini-Projects/08-GrapQL/01-GraphQLByExample/01-FirstProject/server.js
@@ -0,0 +1,18 @@
+const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
+const { makeExecutableSchema } = require('graphql-tools');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const express = require('express');
+const app = express();
+const port = 9000;
+
+const typeDefs = ``
+
+
+
+app.use( cors(), bodyParser.json() );
+
+
+app.listen(port, () => {
+  console.log(`Server running n port: ${port}...`); 
+})
