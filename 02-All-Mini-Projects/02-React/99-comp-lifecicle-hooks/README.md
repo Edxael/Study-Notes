9cc2c0201d43cef4b26fd0c22e677bd4b523612f
Edmundo rubio <edmundo@webaholics.co>
2018-11-02 18:48:35 -0600

    React-Life-Cycles

---
+++ 02-All-Mini-Projects/02-React/99-comp-lifecicle-hooks/README.md
@@ -0,0 +1,6 @@
+# information 
+
+Video-1: React Component Lifecycle - Hooks / Methods Explained
+Location: https://www.youtube.com/watch?v=m_mtV4YaI8c
+
+
---
+++ 02-All-Mini-Projects/02-React/99-comp-lifecicle-hooks/src/App/00-Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Main extends React.Component {
+  render(){
+    return(
+      <div>
+        <p>Main Component</p>
+      </div>
+    )
+  }
+}
+
+export default Main;
