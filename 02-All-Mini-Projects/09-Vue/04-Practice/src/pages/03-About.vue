c1c3f6da269d7eb5e2e9d00afd35574e4da9c711
Edmundo rubio <edmundo@webaholics.co>
2018-11-14 10:21:17 -0700

    add vue test code

---
+++ 02-All-Mini-Projects/09-Vue/04-Practice/src/pages/03-About.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="main-container">
+    <h1>About Component</h1>
+
+    <div>
+      <b-button variant="success" title="Title from title attribute">Log-In-77</b-button>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: "About"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.main-container {
+  border: 1px solid blue;
+  background-color: rgb(255, 255, 255);
+  height: 80vh;
+}
+</style>
