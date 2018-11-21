756bb9078b5e4e4ee2ab89df80a8b06f8a96d220
Edmundo rubio <edmundo@webaholics.co>
2018-11-20 23:50:31 -0700

    add style

--- 02-All-Mini-Projects/09-Vue/04-Practice/src/pages/06-Bootstrap1.vue
+++ 02-All-Mini-Projects/09-Vue/04-Practice/src/pages/06-Bootstrap1.vue
@@ -1,9 +1,17 @@
 <template>
-  <div class="container border border-dark p-5">
-    <div class="row">
+  <div class="container border border-dark p-5 loc-cont-06">
+    <div class="row d-flex justify-content-around">
 
-      <div class="col-md-4 border border-dark bg-light">
-        <p>Oll  Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque nam nemo suscipit velit inventore adipisci cum rerum fugit aliquid dolores, fugiat aut at iusto corporis, aspernatur praesentium odit, cupiditate sunt!</p>
+      <div class="col-md-3 col-sm-12 border border-dark bg-light p-2 shadow-lg">
+        <p>01-le3on  Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque nam nemo suscipit velit inventore adipisci cum rerum fugit aliquid dolores, fugiat aut at iusto corporis, aspernatur praesentium odit, cupiditate sunt!</p>
+      </div>
+
+      <div class="col-md-3 col-sm-12 border border-dark bg-light p-2">
+        <p>02-Oll  Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque nam nemo suscipit velit inventore adipisci cum rerum fugit aliquid dolores, fugiat aut at iusto corporis, aspernatur praesentium odit, cupiditate sunt!</p>
+      </div>
+
+      <div class="col-md-3 col-sm-12 border border-dark bg-light p-2">
+        <p>03-Oll  Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque nam nemo suscipit velit inventore adipisci cum rerum fugit aliquid dolores, fugiat aut at iusto corporis, aspernatur praesentium odit, cupiditate sunt!</p>
       </div>
 
     </div>
@@ -17,7 +25,9 @@ export default {
 </script>
 
 <style>
-
+  .loc-cont-06{
+    background-color: rgb(197, 249, 240);
+  }
 </style>
 
 
