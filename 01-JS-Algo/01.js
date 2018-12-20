48ecc6657b1581bc6b871538cfdebf604e6f7c4f
Edmundo rubio <edmundo@webaholics.co>
2018-12-19 23:09:03 -0700

    26-DecimalToBinary

--- 01-JS-Algo/01.js
+++ 01-JS-Algo/01.js
@@ -2,30 +2,50 @@
 
 console.log("Hello world")
 
-let num = 339
-let baseArr = []
-let binCons = []
-let acumulator = 1
 
-while(acumulator <= num){
-  baseArr.push(acumulator)
-  acumulator = acumulator * 2
-}
+let num = 75
+let arr = []
 
-baseArr = baseArr.reverse()
-console.log("BseArray: ", baseArr)
-console.log("BseArray: ", baseArr.length)
-
-for(let x = 0; x <= baseArr.length -1; x++){
-  if(baseArr[x] <= num){
-    binCons.push(1)
-    num = num - baseArr[x]
+while(num >= 1){
+  if(num % 2 === 0){
+    num = num / 2
+    arr.push(0)
   }else{
-    binCons.push(0)
+    num = Math.floor(num / 2 )
+    arr.push(1)
   }
 }
 
-console.log(" ")
-console.log("BinArr: ", binCons)
-console.log("BinArr: ", binCons.length)
+console.log(arr.reverse())
+
+
+
+
+// From Decimal --> Binary   solution1
+// let num = 339
+// let baseArr = []
+// let binCons = []
+// let acumulator = 1
+
+// while(acumulator <= num){
+//   baseArr.push(acumulator)
+//   acumulator = acumulator * 2
+// }
+
+// baseArr = baseArr.reverse()
+// console.log("BseArray: ", baseArr)
+// console.log("BseArray: ", baseArr.length)
+
+// for(let x = 0; x <= baseArr.length -1; x++){
+//   if(baseArr[x] <= num){
+//     binCons.push(1)
+//     num = num - baseArr[x]
+//   }else{
+//     binCons.push(0)
+//   }
+// }
+
+// console.log(" ")
+// console.log("BinArr: ", binCons)
+// console.log("BinArr: ", binCons.length)
 
