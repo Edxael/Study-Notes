1d101172329587c184d352360fd3846a6bab627d
Edmundo rubio <edmundo@webaholics.co>
2018-12-19 23:11:13 -0700

    decimal to binary indepedient file

---
+++ 01-JS-Algo/26-DecimalToBinary.js
@@ -0,0 +1,50 @@
+
+
+console.log("From Decimal to Binary")
+
+let num = 75
+console.log("The number to conver: ", num)
+let arr = []
+
+while(num >= 1){
+  if(num % 2 === 0){
+    num = num / 2
+    arr.push(0)
+  }else{
+    num = Math.floor(num / 2 )
+    arr.push(1)
+  }
+}
+
+console.log(arr.reverse())
+
+
+
+
+// From Decimal --> Binary   solution1
+// let num = 339
+// let baseArr = []
+// let binCons = []
+// let acumulator = 1
+
+// while(acumulator <= num){
+//   baseArr.push(acumulator)
+//   acumulator = acumulator * 2
+// }
+
+// baseArr = baseArr.reverse()
+// console.log("BseArray: ", baseArr)
+// console.log("BseArray: ", baseArr.length)
+
+// for(let x = 0; x <= baseArr.length -1; x++){
+//   if(baseArr[x] <= num){
+//     binCons.push(1)
+//     num = num - baseArr[x]
+//   }else{
+//     binCons.push(0)
+//   }
+// }
+
+// console.log(" ")
+// console.log("BinArr: ", binCons)
+// console.log("BinArr: ", binCons.length)
